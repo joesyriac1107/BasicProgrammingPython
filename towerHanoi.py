10fe9f6ea126e900cd710ea7d981af2afb773243
Joe Syriac <joesyriac1107@gmail.com>
2018-01-14 22:38:46 +0530

    Added Tower Hanni

---
+++ towerHanoi.py
@@ -0,0 +1,8 @@
+def towerHanoi(n,s,t,i):
+    if n is 1:
+        print "Move disk: "+ '1 ' +"from " + s + " to " + t
+    else:
+        towerHanoi(n-1,s,i,t)
+        print "Move disk: "+str(n) +" from " + s + " to " + t
+        towerHanoi(n-1,i,t,s)
+       
